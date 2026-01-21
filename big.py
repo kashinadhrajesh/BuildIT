bd06af349a60326968706f57057e189358f0ea80
kashinadhrajesh <kashinadhrajesh8797@gmail.com>
2026-01-21 17:32:50 +0530

    day1

---
+++ big.py
@@ -0,0 +1,3 @@
+num=int(input("Enter number"))
+if(num>=10):
+    print("Big")
---
+++ break_continue.py
@@ -0,0 +1,12 @@
+for i in range (10):
+    print(i)
+    if i==5:
+        break
+
+
+for i in range (10):
+    
+    if i==5:
+        continue
+    print(i)
+
